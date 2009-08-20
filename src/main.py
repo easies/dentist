71125a8f1597af29934271406670c8c535c84e27
Alex Lee <lee@ccs.neu.edu>
2009-08-20 14:05:07 -0400

    Add OptionParser

--- src/main.py
+++ src/main.py
@@ -4,16 +4,49 @@ import dentist
 import logging
 
 
+def parse():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-n', '--nodaemonize', dest='nodaemonize',
+                      action='store_false', default=True)
+    parser.add_option('-d', '--directory', dest='directory',
+                      metavar='PATH', help='The directory to watch.',
+                      default='/tmp/lee')
+    parser.add_option('-a', '--access_log', dest='access_logs',
+                      action='append', default=[])
+    parser.add_option('-e', '--error_log', dest='error_logs',
+                      action='append', default=[])
+
+    return parser.parse_args()
+
+
 def main():
+    options, args = parse()
+
+    directory = options.directory
+    access_logs = options.access_logs
+    error_logs = options.error_logs
+
+    if len(access_logs) == 0 and len(access_logs) == 0:
+        import sys
+        sys.stderr.write('Must specify at least one log.\n')
+        return
+
     logging.basicConfig(level=logging.DEBUG)
-
-    f1 = './access.log'
-    clr = dentist.CombinedLogReader()
-    fw = dentist.FileWatcher(f1, clr)
-    dw = dentist.DirWatcher('.', [fw])
-
     poller = dentist.Poller()
-    poller.register_file_watcher(fw)
+    clr = dentist.CombinedLogReader()
+
+    fws = []
+    for f in access_logs:
+        fws.append(dentist.FileWatcher(f, clr, poller))
+    for f in error_logs:
+        pass
+    
+    dw = dentist.DirWatcher(directory, fws)
+    
+    if not options.nodaemonize:
+        pass
+
     while True:
         poller.poll()
 
