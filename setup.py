a30e0055f643680e5ac59068ab0b2520994b7c64
Alex Lee <lee@ccs.neu.edu>
2009-08-21 15:56:12 -0400

    Add distutils

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Dentist',
+    version='0.1.0',
+    description='Apache UserDir log separator',
+    author='Alex Lee',
+    author_email='lee@ccs.neu.edu',
+    url='',
+    package_dir={'': 'src'},
+    packages=['dentist'],
+    scripts=['scripts/dentist'],
+    data_files=[],
+)
